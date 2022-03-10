adc47ccca4461a1ba911baa5871ad0d4731333bf
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-03-10 19:14:14 +0300

    5-prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: returns 0 if code executes successfully
+*/
+int main() {
+    printf("With proper grammer, but the outcome is a piece of art, \n");
+    return (0);
+}
