c49d1e9915b3315dd066ab9b8cc16ac2e913ac19
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-03-10 19:19:57 +0300

    5-prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  *
  * Return: returns 0 if code executes successfully
 */
-int main(void) {
+int main(void) 
+{
     printf("with proper grammar, but the outcome is a piece of art, \n");
     return (0);
 }
