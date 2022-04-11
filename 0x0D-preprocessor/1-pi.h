6a40a3b4e96a65ef8850e055ce90a83340c0f189
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-04-11 16:57:50 +0300

    fixing bug

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,5 @@
 #ifndef PI_H
 #define PI_H
 
-/**
- * file - 1-pi.h
- * description: header file that defines a macro named PI as an abbreviation for the token 3.14159265359
- * 
- */
-
 #define PI 3.14159265359
 #endif
