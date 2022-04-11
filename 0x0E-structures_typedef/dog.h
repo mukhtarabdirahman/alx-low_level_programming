0d3aa843e49b80ca2e01c8f0fe265ab0cf2477e3
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-04-11 23:10:01 +0300

    fixing bug

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,11 @@
 #ifndef _DOG_H_
-#define _DOG_H_
-
+#define _DOG_H_ dog.h
 /**
- *struct dog - information about dogs
- *@name: name of dog
- *@age: age of dog
- *@owner: owner of dog
- */
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ **/
 struct dog
 {
 	char *name;
