a5036c3a24de7c4bdbb2317e7c6256bf762430e2
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-04-13 22:03:05 +0300

    fixing bugs

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-void print_name(char *name, void (*f)(char *));
+#ifndef HEADERGUARD
+#define HEADERGUARD
 int _putchar(char c);
-
+void print_name(char *name, void (*f)(char *));
 #endif
