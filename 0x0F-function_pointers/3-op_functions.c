ddba2a3eaf7c9798ae0460a93a2c29cb117c7687
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-04-13 22:46:43 +0300

    fixing bugs

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,72 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "3-calc.h"
 /**
- * op_add - addition
- * @a: sumnd 1
- * @b: sumnd 2
- * Return: rsult
- */
-
+ * op_add - adds two ints
+ * @a: number 1
+ * @b: number 2
+ * Return: sum of the two numbers
+ **/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtraction
- * @a: minuend
- * @b: sustrnd
- * Return: result
- */
-
+ * op_sub - subtract two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result of substraction
+ **/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiplication
- * @a: multp 1
- * @b: multp 2
- * Return: result
- */
-
+ * op_mul - multiply two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result of multiplication
+ **/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - division
- * @a: divisr
- * @b: dividndo
- * Return: reult
- */
-
+ * op_div - divides two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result of divison
+ **/
 int op_div(int a, int b)
 {
-	if (b)
-	{
-		return (a / b);
-	}
-	printf("Error\n");
-	exit(100);
+	if (b == 0)
+		return (-1);
+	return (a / b);
 }
-
 /**
- * op_mod - modulo
- * @a: vaper 1
- * @b: vaper 2
- * Return: result
- */
-
+ * op_mod - calculates mod of two ints
+ * @a: number 1
+ * @b: number 2
+ * Return: mod of 1, 2
+ **/
 int op_mod(int a, int b)
 {
-	if (b)
-	{
-		return (a % b);
-	}
-	printf("Error\n");
-	exit(100);
+	if (b == 0)
+		return (-1);
+	return (a % b);
 }
